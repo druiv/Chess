6ffab3652aa327281fd08fd324ea0fc042b4eaa6
druiv <druiv@ngs.ru>
2018-07-26 10:16:07 +0700

    Переместил checkMove в абстрактный класс

--- src/com/dz/Figure.java
+++ src/com/dz/Figure.java
@@ -9,7 +9,25 @@ public abstract class Figure implements IFigure {
     public int[] to;
 
     public String checkMove(IMoveCoord moveCoord, Board board) {
-        return "";
+        String result = "";
+        boolean isTo = false;
+        int x = from[1];
+        int y = from[0];
+
+        while (!isTo) {
+            x = x + matrix[template][0];
+            y = y + matrix[template][1];
+
+            if (x == to[1] && y == to[0])
+                isTo = true;
+
+            if (!isTo && !jump && !board.desk[y][x].isEmpty()) {
+                result = "Фигура не может перепрыгнуть";
+                break;
+            }
+
+        }
+        return result;
     }
     public String findTemlate(IMoveCoord moveCoord)  {
         return "";
--- src/com/dz/Rook.java
+++ src/com/dz/Rook.java
@@ -2,7 +2,6 @@ package com.dz;
 
 public class Rook extends Figure implements IFigure {
     Rook(IMoveCoord moveCoord) {
-        //super.Figure(moveCoord);
         int matrix[][] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
         int[] from = moveCoord.getFrom();
         int[] to = moveCoord.getTo();
@@ -38,25 +37,7 @@ public class Rook extends Figure implements IFigure {
     }
 
     public String checkMove(IMoveCoord moveCoord, Board board) {
-        String result = "";
-        boolean isTo = false;
-        int x = from[1];
-        int y = from[0];
-
-        while (!isTo) {
-            x = x + matrix[template][0];
-            y = y + matrix[template][1];
-
-            if (x == to[1] && y == to[0])
-                isTo = true;
-
-            if (!isTo && !jump && !board.desk[y][x].isEmpty()) {
-                result = "Фигура не может перепрыгнуть";
-                break;
-            }
-
-        }
-        return result;
+        return super.checkMove(moveCoord, board);
     }
 
 }
